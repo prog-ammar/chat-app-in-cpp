4faedc94c6007c498ed60a1f80cb6c702df27408
prog-ammar <ammarofc2005@gmail.com>
2025-01-26 15:42:10 +0500

    updated

--- chat-client.cpp
+++ chat-client.cpp
@@ -17,31 +17,33 @@ class makeClient
   {
     ip=address;
     port=portt;
+    makeSocket();
   }
   void makeSocket()
   {
     server=socket(AF_INET,SOCK_STREAM,0);
     struct sockaddr_in server_add;
     server_add.sin_family=AF_INET;
-    server_add.sin_port=htons(1235);
-    inet_pton(AF_INET,ip.c_str(),&server_add);
+    server_add.sin_port=htons(port);
+    inet_pton(AF_INET,ip.c_str(),&server_add.sin_addr);
     client=connect(server,(struct sockaddr*)&server_add,sizeof(server_add));
     send_recv();
   }
   void send_recv()
   {
+    int r;
     char buffer[4096];
+    cout<<"Enter : ";
+    cin.getline(buffer, sizeof(buffer));
     do
     {
-        int r=recv(client,buffer,4096,0);
-        cout<<buffer<<endl;
-
-    } while();
+        r=send(client,buffer,4096,0);
+    } while(r!=-1);
     
   }
-}
+};
 
 int main()
 {
-    makeClient server("192.168.1.85",1235);
+    makeClient server("192.168.1.85",1234);
 }
