1ab08d47e90af20eca5b4765a0908830d5f25b89
prog-ammar <ammarofc2005@gmail.com>
2025-01-27 00:39:26 +0500

    added

--- chat-client.cpp
+++ chat-client.cpp
@@ -15,6 +15,7 @@ class makeClient
   int port;
   int server;
   int client;
+  bool same=true;
   makeClient(string address,int portt)
   {
     ip=address;
@@ -29,29 +30,28 @@ class makeClient
     server_add.sin_port=htons(port);
     inet_pton(AF_INET,ip.c_str(),&server_add.sin_addr);
     client=connect(server,(struct sockaddr*)&server_add,sizeof(server_add));
-    sending();
+    string name;
+    cout<<"Enter Your Name : ";
+    getline(cin,name);
+    send(server,name.c_str(),sizeof(name),0);
+    thread recvi(&makeClient::reciving,this,name);
+    recvi.detach();
+    sending(name);
   }
-  void sending()
+  void sending(string name)
   {
-      string name;
-      cout<<"Enter Your Name : ";
-      getline(cin,name);
-      send(server,name.c_str(),sizeof(name),0);
       string str;
-      thread recive(&makeClient::reciving,this,name);
-      recive.detach();
       while(str!="exit")
       {
         cout<<"You : ";
         getline(cin,str);
-        int r=send(server,str.c_str(),sizeof(str),0);
+        int r=send(server,str.c_str(),str.length(),0);
       }
   }
 
   void reciving(string name)
   {
     char buffer[4096];
-    bool same=true;
     int r;
     while(true)
     {
@@ -62,12 +62,17 @@ class makeClient
           if(name[i]!=buffer[i])
           {
             same=false;
+            break;
+          }
+          else
+          {
+            same=true;
           }
        }
        if(!same)
-      {
-          cout<<buffer<<endl; 
-      }  
+       {
+        cout<<"\r"<<buffer<<endl;
+       } 
     }
       
   }
